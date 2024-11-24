176f5d1790a03263b6d7f647558a09bf570a2982
Munshi Muntasir Rifat <rifat2002m@gmail.com>
2024-11-24 23:48:15 +0600

    car interface

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,41 +1,41 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslintPlugin from "@typescript-eslint/eslint-plugin";
-import tsParser from "@typescript-eslint/parser";
-import prettierPlugin from "eslint-plugin-prettier";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslintPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import prettierPlugin from 'eslint-plugin-prettier';
 
 /** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
   {
-    files: ["**/*.{js,mjs,cjs,ts}"],
+    files: ['**/*.{js,mjs,cjs,ts}'],
     languageOptions: {
       parser: tsParser,
       parserOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module",
+        ecmaVersion: 'latest',
+        sourceType: 'module',
       },
       globals: {
         ...globals.node,
       },
     },
     plugins: {
-      "@typescript-eslint": tseslintPlugin,
+      '@typescript-eslint': tseslintPlugin,
       prettier: prettierPlugin,
     },
     rules: {
-      "no-unused-vars": "error",
-      "no-unused-expressions": "error",
-      "prefer-const": "error",
-      "no-console": "warn",
-      "no-undef": "error",
-      "no-var": "error",
-      "@typescript-eslint/no-unused-vars": "error",
-      "prettier/prettier": "error",
+      'no-unused-vars': 'error',
+      'no-unused-expressions': 'error',
+      'prefer-const': 'error',
+      'no-console': 'warn',
+      'no-undef': 'error',
+      'no-var': 'error',
+      '@typescript-eslint/no-unused-vars': 'error',
+      'prettier/prettier': 'error',
     },
-    ignores: ["node_modules/**", "dist/**"],
+    ignores: ['node_modules/**', 'dist/**'],
   },
   {
-    files: ["**/*.js", "**/*.ts"],
+    files: ['**/*.js', '**/*.ts'],
     rules: {
       // Include rules from eslint:recommended and plugin:@typescript-eslint/recommended
       ...pluginJs.configs.recommended.rules,
---
+++ src/modules/cars/car.model.ts
@@ -0,0 +1,16 @@
+// import { model, Schema } from 'mongoose';
+// import { carInterface } from './car.interface';
+
+// const carSchema = new Schema()<carInterface>({
+//   brand: { type: String },
+//   model: { type: String },
+//   year: { type: Number },
+//   price: { type: Number },
+//   category: ['Sedan', 'SUV', 'Truck', 'Coupe', 'Convertible'],
+//   description: { type: String },
+//   quantity: { type: Number },
+//   inStock: { type: Boolean },
+// });
+
+// // eslint-disable-next-line no-undef
+// export const CarsModel = model<Car, carInterface>('Car', carSchema);
