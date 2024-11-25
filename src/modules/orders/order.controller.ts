926922548faf4af8cd57a96243dbf58fdfb1b207
Munshi Muntasir Rifat <rifat2002m@gmail.com>
2024-11-26 03:02:24 +0600

    remove unused

--- src/modules/orders/order.controller.ts
+++ src/modules/orders/order.controller.ts
@@ -2,27 +2,8 @@ import { Request, Response } from 'express';
 import { orderServices } from './order.service';
 import orderValidationSchema from './order.zodvalidation';
 import { z } from 'zod';
+
 // Create Order
-// const createOrder = async (req: Request, res: Response): Promise<void> => {
-//   try {
-//     const orderData = req.body.orders;
-//     const zodOrderData = orderValidationSchema.parse(orderData);
-//     const result = await orderServices.createOrderIntoDb(zodOrderData);
-
-//     res.status(200).json({
-//       message: 'Order created successfully',
-//       success: true,
-//       data: result,
-//     });
-//   } catch (err: unknown) {
-//     const error = err as Error;
-//     res.status(500).json({
-//       success: false,
-//       message: error.message || 'Order post is wrong',
-//     });
-//   }
-// };
-
 const createOrder = async (req: Request, res: Response): Promise<void> => {
   try {
     const orderData = req.body.orders;
